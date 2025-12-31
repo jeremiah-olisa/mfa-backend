be3d257ad65fd375c97a815ba4c0b00cffbc4331
Jeremiah Olisa <jeremiaholisa453@gmail.com>
2025-12-31 17:21:38 +0100

    feat: Integrate Tailwind CSS into the Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import { defineConfig } from 'vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
+        tailwindcss(),
         laravel({
             input: 'resources/js/app.ts',
             refresh: true,
